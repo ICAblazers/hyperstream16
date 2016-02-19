1161d6535c8f2856551583516a3d1a337eabb2ef
BoidBoid <bhall@icablazers.org>
2016-02-18 22:41:54 -0600

    Screwing around with Bootstrap and jQuery.

--- javascript/MainScript.js
+++ javascript/MainScript.js
@@ -1,3 +1,14 @@
 /**
  * Created by admin on 2/18/16.
  */
+
+
+$("#warning-btn").click(function() {
+        $("#warning-btn").animate(
+            {
+                opacity: 'hide',
+                height: 'hide'
+            }
+            , 'slow');
+    }
+);
