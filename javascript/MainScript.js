4c5cad898d7da3de1969ef79a08586da3691d849
BoidBoid <bhall@icablazers.org>
2016-02-19 23:45:55 -0600

    Cleaning up the header and jumbotron

--- javascript/MainScript.js
+++ javascript/MainScript.js
@@ -15,4 +15,16 @@ $(document).ready(function(){
                 , 'fast');
         }
     );
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1){
+            $('#header').addClass("header-sticky");
+            $('.page-header').addClass("page-header-alt");
+        }
+        else{
+            $('#header').removeClass("header-sticky");
+            $('.page-header').removeClass("page-header-alt");
+
+        }
+    });
 });
