10c5d3f922392cedd7143f66cb23d318ecb456d4
BoidBoid <bhall@icablazers.org>
2016-02-20 09:43:00 -0600

    Edited header animations

--- javascript/MainScript.js
+++ javascript/MainScript.js
@@ -3,6 +3,8 @@
  */
 
 $(document).ready(function(){
+    var header_default_html = $('#header-row').html();
+    var header_default_height = $('#header').css('height');
     $('p').click(function(){
         $(this).hide();
     });
@@ -17,12 +19,14 @@ $(document).ready(function(){
     );
 
     $(window).scroll(function() {
-        if ($(this).scrollTop() > 1){
-            $('#header').addClass("header-sticky");
+        if ($(this).scrollTop() > 5){
+            $('#header').addClass("header-sticky").animate({height: '16px'}, 'fast');
+            $('#header-row').html('<button class="btn btn-default col-xs-1 col-xs-offset-11"></button>');
             $('.page-header').addClass("page-header-alt");
         }
         else{
-            $('#header').removeClass("header-sticky");
+            $('#header').animate({height: header_default_height}, 'fast').removeClass("header-sticky");
+            $('#header-row').html(header_default_html);
             $('.page-header').removeClass("page-header-alt");
 
         }
