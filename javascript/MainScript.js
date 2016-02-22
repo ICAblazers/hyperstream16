29b92a0e976ef2acb56da9ad71d060c1b59ad8cf
Bryce Hall <bhall@icablazers.org>
2016-02-22 15:36:53 -0600

    ttttttt

--- javascript/MainScript.js
+++ javascript/MainScript.js
@@ -18,17 +18,28 @@ $(document).ready(function(){
         }
     );
 
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 1){
-            $('#header').stop().addClass("header-sticky").animate({height: '38px'}, 'fast');
-            $('#header-row').html('<button class="btn btn-default col-xs-1 col-xs-offset-11 menu-btn"><img src="../images/menu.png"></button>');
-            $('.page-header').addClass("page-header-alt");
-        }
-        else{
-            $('#header').stop().animate({height: header_default_height}, 'fast').removeClass("header-sticky");
-            $('#header-row').html(header_default_html);
-            $('.page-header').removeClass("page-header-alt");
 
+
+
+    var compact_header = false;
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1 && compact_header == false){
+            compact_header = true;
+            $('#header-filler').addClass("header-filler");
+            $('#header').stop(true,true).addClass("header-sticky").animate({height: '0'}, {queue:false, duration:'fast', complete:function(){
+                $('#header-row').html('<button class="btn btn-default col-xs-1 col-xs-offset-11 menu-btn"><img src="../images/menu.png"></button>');
+                $('#header').animate({height: '38px'}, {queue:false, duration:'fast'});
+            }});
+        }
+        else if ($(this).scrollTop() == 0 && compact_header == true){
+            compact_header = false;
+            $('#header').stop(true,true).animate({height: '0'}, {queue:false, duration:'fast', complete:function(){
+                $('#header-row').html(header_default_html);
+                $('#header').animate({height: header_default_height}, {queue:false, duration:'fast', complete:function(){
+                    $('#header').removeClass("header-sticky");
+                    $('#header-filler').removeClass("header-filler");
+                }});
+            }})
         }
     });
 });
