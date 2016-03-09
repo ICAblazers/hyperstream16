d66e95e069b75f097b457ebb38340bf1ff3737d2
Malik Mcgregor <mmcgregor@icablazers.org>
2016-03-09 14:59:48 -0600

    CSS edits

--- javascript/MainScript.js
+++ javascript/MainScript.js
@@ -29,3 +29,5 @@ $(document).ready(function(){
 
     })
 });
+function init_map(){var myOptions = {zoom:10,center:new google.maps.LatLng(51.5073509,-0.12775829999998223),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(51.5073509,-0.12775829999998223)});infowindow = new google.maps.InfoWindow({content:'<strong>Title</strong>' +
+'<br>London, United Kingdom<br>'});google.maps.event.addListener(marker, 'click', function(){infowindow.open(map,marker);});infowindow.open(map,marker);}google.maps.event.addDomListener(window, 'load', init_map);
