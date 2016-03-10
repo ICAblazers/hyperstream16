d5704f110cdeee28e2aa4bed7f7a440b7b0a9132
Bryce Hall <bhall@icablazers.org>
2016-03-10 14:44:56 -0600

    Merged branches

--- javascript/MainScript.js
+++ javascript/MainScript.js
@@ -21,6 +21,8 @@ $(document).ready(function(){
             $('#header-fixed').stop(true,true).animate({height: '0'}, {queue:false, duration:'fast', easing:'linear', complete:function(){
                 $('#nav-default').removeClass("invis");
                 $('#nav-compressed').addClass("invis");
+                $('.dropdown').removeClass("open");
+                $(".menu-btn").blur();
                 $('#header-fixed').animate({height: header_default_height}, {queue:false, duration:'fast', complete:function(){
                     $('#header-fixed').removeClass("header-sticky");
                 }});
@@ -32,6 +34,8 @@ $(document).ready(function(){
     })
 });
 
-
 function init_map(){var myOptions = {zoom:10,center:new google.maps.LatLng(51.5073509,-0.12775829999998223),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(51.5073509,-0.12775829999998223)});infowindow = new google.maps.InfoWindow({content:'<strong>Title</strong>' +
-'<br>London, United Kingdom<br>'});google.maps.event.addListener(marker, 'click', function(){infowindow.open(map,marker);});infowindow.open(map,marker);}google.maps.event.addDomListener(window, 'load', init_map);
+'<br>London, United Kingdom<br>'});google.maps.event.addListener(marker, 'click', function(){infowindow.open(map,marker);});infowindow.open(map,marker);
+}
+
+google.maps.event.addDomListener(window, 'load', init_map);
