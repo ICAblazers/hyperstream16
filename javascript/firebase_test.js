485e4da08dfdee2c73d0f77432f430c1adc72434
Malik Mcgregor <mmcgregor@icablazers.org>
2016-04-07 15:04:43 -0500

    CSS edits

--- javascript/firebase_test.js
+++ javascript/firebase_test.js
@@ -2,7 +2,7 @@
  * Created by admin on 3/30/16.
  */
 // Create a callback which logs the current auth state
-$(document).ready(function() {
+$(document).ready(function () {
     function authDataCallback(authData) {
         if (authData) {
             console.log("User " + authData.uid + " is logged in with " + authData.provider);
@@ -15,11 +15,21 @@ $(document).ready(function() {
     var ref = new Firebase("https://sizzling-fire-7136.firebaseio.com");
     ref.onAuth(authDataCallback);
 
-    $("#register-submit").onclick = function(){
+    $("#register-submit").on('click', function () {
+        var firstName = $("#firstname")[0].value;
+        var lastName = $("#lastname")[0].value;
+        var userName = $("#username")[0].value;
+        var email = $("#email")[0].value;
+        var pass = $("#reg-password")[0].value;
+        var passConfirm = $("#confirm-password")[0].value;
+
+        var inputData = [firstName, lastName, userName, email, pass, passConfirm];
+
+        if (firstName && lastName && userName && email && pass && passConfirm && pass == passConfirm) {
             ref.createUser({
-                email: $("#email").value,
-                password: $("#reg-password").value
-            }, function(error, userData) {
+                "email": email,
+                "password": pass
+            }, function (error, userData) {
                 if (error) {
                     switch (error.code) {
                         case "EMAIL_TAKEN":
@@ -35,11 +45,37 @@ $(document).ready(function() {
                     console.log("Successfully created user account with uid:", userData.uid);
                 }
             });
+        } else {
 
+        }
+    });
+    var inputError=function(inputData){
+        var outputA = "Please fill out the required fields: \n";
+        var outputB;
+        var output;
 
+        switch (false) {
+            case inputData[0]:
+                outputA += "     First Name \n";
+            case inputData[1]:
+                outputA += "     Last Name \n";
+            case inputData[2]:
+                outputA += "     Username \n";
+            case inputData[3]:
+                outputA += "     Email \n";
+            case inputData[4]:
+                outputA += "     Password \n";
+            case inputData[5]:
+                outputA += "     Confirm Password \n";
+            case inputData[4] == inputData[5]:
+                outputB += "\nPlease make sure the Password and Confirm Password fields match.";
+                break;
+            default:
+                outputA = "An error occurred.  Please make sure the fields are correctly filled and try again";
+                break;
 
+            output = outputA + outputB;
+            return output;
+        }
     }
-
-
-    }
-)
+});
