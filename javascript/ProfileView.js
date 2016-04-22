1f802da7de89eb7390e1791c3bf74718df11d7ef
Bryce Hall <bhall@icablazers.org>
2016-04-22 15:04:27 -0500

    Merged branches

--- javascript/ProfileView.js
+++ javascript/ProfileView.js
@@ -3,7 +3,9 @@
  */
 $(document).ready(function() {
     function loadData() {
-            var userRef = new Firebase("https://ica-stem16.firebaseio.com/users/" + window.location.hash);
+        id = getQueryVariable("id");
+        if(id){
+            var userRef = new Firebase("https://ica-stem16.firebaseio.com/users/" + id);
             userRef.on("value", function(snapshot) {
                 console.log(snapshot.val());
                 $("#name").html(snapshot.val().firstName + " " + snapshot.val().lastName);
@@ -11,8 +13,21 @@ $(document).ready(function() {
                 console.log("The read failed: " + errorObject.code);
                 window.location.assign('../html/Main.html');
             });
-        };
+        } else {
+            window.location.assign('../html/Main.html');
+        }
+    };
 
 
     loadData();
 });
+function getQueryVariable(variable)
+{
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+        var pair = vars[i].split("=");
+        if(pair[0] == variable){return pair[1];}
+    }
+    return(false);
+}
